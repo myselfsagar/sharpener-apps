13ef05b6ae23fa3f46a924b2e3c92a1e99c97af1
myselfsagar <ssahu6244@gmail.com>
2025-04-22 13:03:49 +0530

    expense app - Creating Login Screen Frontend

--- Major projects/Expense Tracker/client/pages/login/login.js
+++ Major projects/Expense Tracker/client/pages/login/login.js
@@ -1,3 +1,19 @@
-function handleLoginForm(event) {
+const SERVER_BASE_URL = "http://localhost:4000";
+
+async function handleLoginForm(event) {
   event.preventDefault();
+
+  //get the input values
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+
+  try {
+    const response = await axios.post(`${SERVER_BASE_URL}/user/login`, {
+      email,
+      password,
+    });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
 }
