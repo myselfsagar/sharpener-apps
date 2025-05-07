7ddd2b38afe1d3d379e44aef36df3bd60dcd1a75
myselfsagar <ssahu6244@gmail.com>
2025-05-08 02:04:22 +0530

    chnaged code for rds

--- Major projects/Expense Tracker/server/config/dbConfig.js
+++ Major projects/Expense Tracker/server/config/dbConfig.js
@@ -1,16 +1,21 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("expense_app", "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: process.env.DIALECT,
+  }
+);
 
 (async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
   } catch (err) {
-    console.error("Unable to connect to the database:", error);
+    console.error("Unable to connect to the database:", err);
   }
 })();
 
